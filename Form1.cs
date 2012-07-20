f328bef7d83f5be1f1f938db08ecbe2dc7a9bbdc
Dejan Jankovic <dejo.janko@gmail.com>
2012-07-20 16:19:13 +0200

    Prva klasa u windows aplikaciji

--- Form1.cs
+++ Form1.cs
@@ -6,9 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using Biblioteka;
 
-namespace APLIKACIJA
+namespace WindowsAplikacija
 {
     public partial class Form1 : Form
     {
@@ -17,29 +16,44 @@ namespace APLIKACIJA
             InitializeComponent();
         }
 
-
-       
-
         private void button1_Click(object sender, EventArgs e)
         {
             double tezina = Double.Parse(textBox1.Text);
             double visina = Double.Parse(textBox2.Text);
 
-            double rezultat=Biblioteka.BMIKalkulator.IzracunajBMI(tezina, visina);
-           
-           
+            double rezultat = Biblioteka.BMIKalkulator.IzracunajBMI(visina,tezina);
+
+            textBox3.Text = Biblioteka.BMIKalkulator.ispisiKategoriju(rezultat).ToString();
+
+            if (rezultat <= 15 && rezultat >= 0)
+            {
+                textBox3.Text += " ---> vi pripadate grupi: ANOREKSICNI.";
+            }
+
+            if (rezultat > 15 && rezultat <= 18.5)
+            {
+                textBox3.Text += " ---> vi pripadate grupi: MRSAVI.";
+            }
+
+            if (rezultat > 18.5 && rezultat <=25)
+            {
+                textBox3.Text += " ---> vi pripadate grupi: NORMALNI.";
+            }
+
+            if (rezultat > 25 && rezultat <= 30)
+            {
+                textBox3.Text += " ---> vi pripadate grupi: POPUNJENI.";
+            }
+
+            if (rezultat > 30 && rezultat <=40)
+            {
+                textBox3.Text += " ---> vi pripadate grupi: DEBELI.";
+            }
+
+            if (rezultat >40)
+            {
+                textBox3.Text += " ---> vi pripadate grupi: GOJAZNI.";
+            }
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-    
-
-        
-       
-
-        
     }
 }
